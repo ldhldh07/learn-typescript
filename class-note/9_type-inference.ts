1a83a089281bc661289ad877fad4467d0170f840
임두현 <dhlim07@naver.com>
2023-04-25 14:41:46 +0900

    add section 11 note

---
+++ class-note/9_type-inference.ts
@@ -0,0 +1,46 @@
+// 타입 추론 기본 1
+
+var a = 'abc';
+// 기본적인 변수 할당 시 추론이 됨
+
+// 기본값 설정시에도 추론
+function getA(b = 10) {
+  var c = 'hi';
+  return b + c;
+}
+
+// 타입 추론 기본 2
+interface Dropdown<T> {
+  value: T;
+  title: string;
+}
+
+var shoppingItem: Dropdown<string> = {
+  value: 'abc',
+  title: 'hello',
+}
+
+// 타입 추론 기본 3
+interface Dropdown<T> {
+  value: T;
+  title: string;
+}
+
+interface DetailedDropdown<K> extends Dropdown<K> {
+  description: string;
+  tag: K;
+}
+
+// 디테일드 드랍다운 K로 가서 그게 드랍다운 K로 감
+
+var detailedItem: DetailedDropdown<string> = {
+  title: 'abc',
+  description: 'ab',
+  value: 'a',
+  tag: 'a',
+}
+
+var shoppingItem: Dropdown<string> = {
+  value: 'abc',
+  title: 'hello',
+}
